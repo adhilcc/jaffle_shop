9d391450f56f1836d315fb84dfc809256d4f08bf
Akshaidevopscc <148193870+Akshaidevopscc@users.noreply.github.com>
2024-10-31 02:37:55 +0530

    Update template_DAG.py

--- SCHEMA_A/template_DAG.py
+++ SCHEMA_A/template_DAG.py
@@ -45,8 +45,6 @@ params = {}
 for param_key in config.get('PARAMS', []):
     params[param_key] = Variable.get(param_key)
 
-TAGS.extend(params.get('r2', []))
-
 # Initialize the DAG
 dag = DAG(
     dynamic_dag_id,
